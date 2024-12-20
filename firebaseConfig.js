ac1e288b492ac974814c49569a7fb5f1deff3fbf
ValhallaAMB <you@example.com>
2024-12-20 22:05:39 +0300

    Installed dependencies

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDauojnIvl2cQwEceqnr-l-Uc1PvclYjbM",
+  authDomain: "fir-expo-test-2a4d9.firebaseapp.com",
+  projectId: "fir-expo-test-2a4d9",
+  storageBucket: "fir-expo-test-2a4d9.firebasestorage.app",
+  messagingSenderId: "310213429864",
+  appId: "1:310213429864:web:b28ce503a3f9212ce809dd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
